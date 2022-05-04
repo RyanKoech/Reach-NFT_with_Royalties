439034159ca4dcbea51883f422ba28b018e784ce
Ryan Koech <sirryankoech@gmail.com>
2022-05-04 12:42:33 +0300

    Initial Set Up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import {loadStdlib} from '@reach-sh/stdlib'
+const reach = loadStdlib('ALGO');
+reach.setProviderByName("LocalHost")
+const startingBalance = reach.parseCurrency(100);
+
 
 function App() {
+  const [accountDetails, setAccountDetails] = useState({})
+
+  async function connectWallet() {
+    console.log("Waiting...")
+    const acc = await reach.newTestAccount(startingBalance);
+    const accAdd = acc.getAddress();
+    const balAtomic = await reach.balanceOf(acc);
+    const bal = reach.formatCurrency(balAtomic, 4);
+    setAccountDetails({
+      accountAddress: accAdd,
+      accountBalance: bal
+    });
+    if (await reach.canFundFromFaucet()) {
+      console.log("Can Fund from Faucet boys!")
+    } else {
+      console.log("Ahhh shit here we go again.")
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +34,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          <button type="button" onClick={connectWallet}>
+            Connect
+          </button>
+        </p>
+        <div>
+          <ul>
+            <li>Account Address: {accountDetails.accountAddress}</li>
+            <li>Account Balance: {accountDetails.accountBalance}</li>
+          </ul>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
