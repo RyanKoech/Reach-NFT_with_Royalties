1d700900dd984eb3021947eacec49a89dbd74127
Ryan Koech <sirryankoech@gmail.com>
2022-05-04 14:22:39 +0300

    Added Components

---
+++ src/components/MainApp.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MainApp = () => {
+  return(
+    <div> This is where the main app should be</div>
+  );
+}
+
+export default MainApp;
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div> This Should be a navbar </div>
+  );
+}
+
+export default Navbar;
