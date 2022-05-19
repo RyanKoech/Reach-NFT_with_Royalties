ca6f15605edbdeb784c31161e7971a026de5b965
Jomah <gicugumajoseph@gmail.com>
2022-05-19 22:15:30 +0300

    Here is the frontend

--- src/components/participants/CreatorViews.js
+++ src/components/participants/CreatorViews.js
@@ -21,18 +21,57 @@ const CreateNFT = ({deployContract}) => {
   }
 
   return(
-    <div>
-      <form action="">
-        <label htmlFor="">ID</label>
-        <input type="number" min={100} step="1" onChange={(e)=>{setNftId(e.target.value)}}/>
-        <label htmlFor="">BasePrice</label>
-        <input type="number" min={0.001} step="0.001" onChange={(e)=>{setBasePrice(e.target.value)}}/>
-        <label htmlFor="">Royalty</label>
-        <input type="number" min={0} step="0.1" max={49} onChange={(e)=>{setRoyalty(e.target.value)}}/>
-        <label htmlFor="">Metadata</label>
-        <input type="text" onChange={(e)=>{setMetaData(e.target.value)}}/>
+    <div className="antaliased">
+      <div className="flex w-full min-h-screen justify-center items-center">
+        <div className="flex flex-col space-y-6 bg-cyan-700 w-50% maw-w-4xl p-8 rounded-xl shadow-lg teal-white">
+        
+          <div className="flex flex-col justify-between">
+            <div>
+              <h1 className="font-bold text-4xl tracking-wide">Welocme to the creator side</h1>
+              <p className="pt-2 text-cyan-100 text-sm">Start</p>
+            </div>
+          </div>
+
+          <div>
+        <div className="bg-white rounded-xl shadow-lg p-8 ">
+        <form action="" className="flex flex-col space-y-4">
+        <div>
+        <label htmlFor="" className="tet-sm">ID</label>
+        </div>
+        <div>
+        <input type="number" placeholder="Your ID" className="ring-1 ring-gray-500 w-full rounded-md px-4 py-2 outline-none mt-2 focus:ring-2 focus:ring-teal-300" min={100} step="1" onChange={(e)=>{setNftId(e.target.value)}}/>
+        </div>
+        <div>
+        <label htmlFor="" className="tet-sm">BasePrice</label>
+        </div>
+        <div>
+        <input type="number" placeholder="Base price" className="ring-1 ring-gray-500 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300" min={0.001} step="0.001" onChange={(e)=>{setBasePrice(e.target.value)}}/>
+        </div>
+        <div>
+        <label htmlFor="" className="tet-sm">Royalty</label>
+        </div>
+        <div>
+        <input type="number" placeholder="Royalty" className="ring-1 ring-gray-500 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300" min={0} step="0.1" max={49} onChange={(e)=>{setRoyalty(e.target.value)}}/>
+        </div>
+        <div>
+        <label htmlFor="" className="tet-sm">Metadata</label>
+        </div>
+        <div>
+        <input type="text" placeholder="Metadata" className="ring-1 ring-gray-500 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300" onChange={(e)=>{setMetaData(e.target.value)}}/>
+        </div>
+        
       </form>
-      <button onClick={handleSubmit}>Create</button>
+      <div>
+      <button className="inline-blovk self-end bg-cyan-700 text-white font-bold rounded-lg px-6 py-2 uppercase text-sm" onClick={handleSubmit}>Create</button>
+      </div>
+      
+            </div>
+          
+          </div>
+       
+        </div>
+      </div>
+      
     </div>
   )
 }
@@ -43,16 +82,29 @@ const ShowContractInfo = ({contractInfo, setHasShownContractInfo}) => {
   const handleCopy = () => { setCopied(true) };
 
   return(
-    <div>
+    <div className="flex w-full min-h-screen justify-center items-center">
+      <div className="flex flex-col space-y-6 bg-cyan-700 w-50% maw-w-4xl p-8 rounded-xl shadow-lg teal-white">
+      <div>
       <pre className="mt-4 p-4">
         {contractInfo}
       </pre>
-      <CopyToClipboard onCopy={handleCopy} text={contractInfo}>
-          <button variant="primary">
+      </div>
+
+      <div>
+      <div>
+        <CopyToClipboard onCopy={handleCopy} text={contractInfo}>
+          <button className="inline-blovk self-end bg-cyan-700 text-white font-bold rounded-lg px-6 py-2 uppercase text-sm" variant="primary">
               {copied ? "Copied" : "Copy to clipboard"}
           </button>
       </CopyToClipboard>
-      <div><button onClick={setHasShownContractInfo}>Continue to Begin Auction</button></div>
+      </div>
+      
+      <div><button className="inline-blovk self-end bg-cyan-700 text-white font-bold rounded-lg px-6 py-2 uppercase text-sm" onClick={setHasShownContractInfo}>Continue to Begin Auction</button></div>
+      </div>
+      </div>
+      
+
+      
     </div>
   )
 }
